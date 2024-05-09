0b31b26966444234fd8dc7fd7387be1ba3ad2f38
yhoyo-stelk08 <eko.prasetyo.38013@gmail.com>
2024-05-10 07:45:26 +0800

    feat: install react-spinners instead. - uninstall react-spinner and install react spinners. - remove the line that mock loading behaviour.
     Changes to be committed:
    	modified:   db_query/meals.js
    	modified:   package-lock.json
    	modified:   package.json

--- db_query/meals.js
+++ db_query/meals.js
@@ -2,7 +2,7 @@ import db from '@/database';
 
 export const fetchAllMeals = async () => {
   // mocking a loading
-  await new Promise((resolve) => setTimeout(resolve, 2000));
+//   await new Promise((resolve) => setTimeout(resolve, 5000));
   const result = db.query('SELECT * FROM meals ORDER BY id');
   return (await result).rows;
 };
