a478f77fb21a56c2e1089c4152b0b2f372d0fede
yhoyo-stelk08 <eko.prasetyo.38013@gmail.com>
2024-05-09 20:27:29 +0800

    feat: create new components MealsGrid.jsx. create new components MealsGrid.jsx and do some styling with tailwind.
     Changes to be committed:
    	new file:   components/MealsGrid.jsx

---
+++ components/MealsGrid.jsx
@@ -0,0 +1,14 @@
+const MealsGrid = ({ meals }) => {
+  const ulClass = `grid grid-cols-[repeat(auto-fill,minmax(20rem,1fr))] w-[90%] max-w-[90rem] gap-20 my-8 mx-auto p-0 `;
+  return (
+    <ul
+      className={`${ulClass}`}
+    >
+      {meals.map((meal) => (
+        <li key={meal.id}></li>
+      ))}
+    </ul>
+  );
+};
+
+export default MealsGrid;
