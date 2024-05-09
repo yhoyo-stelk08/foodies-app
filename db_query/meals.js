83f967929963b04bb667af60afc1d18ec7ef1d00
yhoyo-stelk08 <eko.prasetyo.38013@gmail.com>
2024-05-10 07:35:52 +0800

    feat: create loading page & install react-spinner - install react-spinner. - fix the security bugs. - mock the loading behaviour in meals.js. - create loading page and use react-spinner in loading page.
     Changes to be committed:
    	new file:   app/loading.jsx
    	modified:   db_query/meals.js
    	modified:   package-lock.json
    	modified:   package.json

--- db_query/meals.js
+++ db_query/meals.js
@@ -1,6 +1,8 @@
 import db from '@/database';
 
 export const fetchAllMeals = async () => {
+  // mocking a loading
+  await new Promise((resolve) => setTimeout(resolve, 2000));
   const result = db.query('SELECT * FROM meals ORDER BY id');
   return (await result).rows;
 };
