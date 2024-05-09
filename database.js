071483b9acda2f16c00d51e7c4f4a40250884e98
yhoyo-stelk08 <eko.prasetyo.38013@gmail.com>
2024-05-09 22:56:58 +0800

    feat: using Client instead Pool class.

--- database.js
+++ database.js
@@ -1,11 +1,17 @@
-import { Pool } from 'pg';
+import { Client } from 'pg';
 
-const conn = new Pool({
+const conn = new Client({
     user: process.env.PG_USER,
     host: process.env.PG_HOST,
     database: process.env.PG_DBNAME,
     password: process.env.PG_PASSWORD,
     port: process.env.PG_PORT,
 });
+
+try {
+    conn.connect()
+} catch (error) {
+    console.log('Failed to connect to database: ',error)
+}
 
 export default conn;
