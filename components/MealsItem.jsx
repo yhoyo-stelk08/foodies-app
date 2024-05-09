5b6a69b75a3b59eba40027220ae507fa83c65ad1
yhoyo-stelk08 <eko.prasetyo.38013@gmail.com>
2024-05-09 20:43:00 +0800

    feat: format the linting.

--- components/MealsItem.jsx
+++ components/MealsItem.jsx
@@ -1,8 +1,8 @@
 import Link from 'next/link';
 import Image from 'next/image';
 
-const MealsItem = ({title, slug, image, summary, creator}) => {
-    const mealClass = `flex flex-col justify-between h-full rounded overflow-hidden shadow-xl transition-all duration-300 ease-in-out text-amber-100 bg-gradient-to-r from-amber-950 to-yellow-950`
+const MealsItem = ({ title, slug, image, summary, creator }) => {
+  const mealClass = `flex flex-col justify-between h-full rounded overflow-hidden shadow-xl transition-all duration-300 ease-in-out text-amber-100 bg-gradient-to-r from-amber-950 to-yellow-950`;
   return (
     <article className={`${mealClass}`}>
       <header>
@@ -10,7 +10,7 @@ const MealsItem = ({title, slug, image, summary, creator}) => {
           <Image src={image} alt={title} fill />
         </div>
         <div className={`p-[0.5rem 1rem 0 1rem]`}>
-          <h2 className='m-0 text-2xl font-Montserrat'>{title}</h2>
+          <h2 className="m-0 text-2xl font-Montserrat">{title}</h2>
           <p className={`italic text-xs text-red-200`}>by {creator}</p>
         </div>
       </header>
@@ -21,7 +21,7 @@ const MealsItem = ({title, slug, image, summary, creator}) => {
         </div>
       </div>
     </article>
-  )
-}
+  );
+};
 
-export default MealsItem
+export default MealsItem;
