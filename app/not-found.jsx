7a1cf568aab6e483a033177579d8d1f7df7e7070
yhoyo-stelk08 <eko.prasetyo.38013@gmail.com>
2024-05-10 09:29:17 +0800

    feat: create not found page. create not found page and put it in root folder so it can catch all not found error occured in all page.
     Changes to be committed:
    	new file:   app/not-found.jsx

---
+++ app/not-found.jsx
@@ -0,0 +1,10 @@
+const NotFoundPage = () => {
+  return (
+    <main className="not-found">
+      <h1>Page not found</h1>
+      <p>Unfortunately, we could not find the requested page or resource.</p>
+    </main>
+  );
+};
+
+export default NotFoundPage;
