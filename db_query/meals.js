0f0cff6b75b4e8bb1512ab7f0383c4006b5f7ee2
yhoyo-stelk08 <eko.prasetyo.38013@gmail.com>
2024-05-10 23:52:54 +0800

    feat: create function to save meal to database. - create function saveMeal to save meal to database in meals.js files. - import saveMeal function and use it in actions.js files. - redirect user to meals page after save the meal recipe. - test the saveMeal function.
     Changes to be committed:
    	modified:   db_query/meals.js
    	modified:   lib/actions.js
        new file:   public/images/coto-makassar-2397e350-ffa9-4990-b86f-40954c6d12e2.jpg

--- db_query/meals.js
+++ db_query/meals.js
@@ -1,4 +1,8 @@
 import db from '@/database';
+import slugify from 'slugify';
+import xss from 'xss';
+import { v4 as uuidv4 } from 'uuid';
+import fs from 'node:fs';
 
 export const fetchAllMeals = async () => {
   // mocking a loading
@@ -10,4 +14,37 @@ export const fetchAllMeals = async () => {
 export const fetchMeal = async (slug) => {
   const result = await db.query('SELECT * FROM meals WHERE slug = $1', [slug]);
   return result.rows[0];
+};
+
+export const saveMeal = async (meal) => {
+  meal.slug = slugify(meal.title, { lower: true });
+  meal.instruction = xss(meal.instruction);
+
+  // get image extension
+  const extension = meal.image.name.split('.').pop();
+  const uniqId = uuidv4();
+  const fileName = `${meal.slug}-${uniqId}.${extension}`
+
+  // save image to file system
+  const stream = fs.createWriteStream(`public/images/${fileName}`);
+  const bufferedImage = await meal.image.arrayBuffer();
+
+  // writing image to filesystem
+  stream.write(Buffer.from(bufferedImage), (error) => {
+    if(error) {
+      throw new Error('Saving image failed!!')
+    }
+  });
+  // re assign the meal.image to the path of filename
+  meal.image = `/images/${fileName}`
+
+  // save meal to database table.
+  try {
+    db.query(`
+    INSERT INTO meals (slug, title, image, summary, instruction, creator, creator_email) VALUES 
+    ($1, $2, $3, $4, $5, $6, $7)
+    `,[meal.slug, meal.title, meal.image, meal.summary, meal.instruction, meal.creator, meal.creator_email]);
+  } catch (error) {
+    console.log('Error occured when trying to saving meal to database table', error);
+  }
 };
