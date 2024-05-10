14c1f9f92b73e42b79488d87d537c69da7e5974a
yhoyo-stelk08 <eko.prasetyo.38013@gmail.com>
2024-05-10 17:01:49 +0800

    feat: create a function to show image preview.
     Changes to be committed:
    	modified:   components/ImagePicker.jsx

--- components/ImagePicker.jsx
+++ components/ImagePicker.jsx
@@ -1,18 +1,45 @@
 'use client';
-import { useRef } from 'react';
+import { useRef, useState } from 'react';
 import classes from './ImagePicker.module.css';
+import Image from 'next/image';
 
 const ImagePicker = ({ label, name }) => {
+  const [pickedImage, setPickedImage] = useState();
   const imageInput = useRef();
+
   const handlePickImg = () => {
     imageInput.current.click();
   };
+
+  const handleImageChange = (event) => {
+    const file = event.target.file[0];
+
+    if (!file) {
+      setPickedImage(null);
+      return;
+    }
+
+    const fileReader = new FileReader();
+
+    fileReader.onload = () => {
+      setPickedImage(fileReader.result);
+    };
+    fileReader.readAsDataURL(file);
+  };
+
   return (
     <div className={classes.picker}>
       <label htmlFor={name}>{label}</label>
       <div className={classes.controls}>
+        <div className={classes.preview}>
+          {!pickedImage && <p>No image picked yet.</p>}
+          {pickedImage && (
+            <Image src={pickedImage} alt="The image selected by user" fill />
+          )}
+        </div>
         <input
           className={classes.input}
+          onChange={handleImageChange}
           type="file"
           id={name}
           accept="image/png, image/jpeg"
