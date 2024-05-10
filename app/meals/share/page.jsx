a788a9547601c8574848e88ed728770efcdc39a8
yhoyo-stelk08 <eko.prasetyo.38013@gmail.com>
2024-05-11 00:02:51 +0800

    feat: create new components MealsFormSubmit.jsx. - create new components MealsFormSubmit.jsx - use useFormState hooks from react-dom - add condtional rendering based on pending status. - use the new components in Meals Page.
     Changes to be committed:
    	modified:   app/meals/share/page.jsx
    	new file:   components/MealsFormSubmit.jsx

--- app/meals/share/page.jsx
+++ app/meals/share/page.jsx
@@ -1,6 +1,7 @@
-import ImagePicker from '@/components/ImagePicker';
 import classes from './page.module.css';
 import { shareMeal } from '@/lib/actions';
+import ImagePicker from '@/components/ImagePicker';
+import MealsFormSubmit from '@/components/MealsFormSubmit';
 
 const MealsSharePage = () => {
   return (
@@ -42,7 +43,7 @@ const MealsSharePage = () => {
           </p>
           <ImagePicker label='Your image' name='image' />
           <p className={classes.actions}>
-            <button type="submit">Share Meal</button>
+            <MealsFormSubmit />
           </p>
         </form>
       </main>
