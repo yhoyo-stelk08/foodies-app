dc4f9832b5ebe1687bc38e0d33db1892e55ebe24
yhoyo-stelk08 <eko.prasetyo.38013@gmail.com>
2024-05-10 10:27:08 +0800

    feat: modified app/meals/[slug]/page.jsx - modified app/meals/[slug]/page.jsx. - create new file page.module.css and use tailwind style in it. - modified tailwind.config.js add custom animation.
     Changes to be committed:
    	modified:   app/meals/[slug]/page.jsx
    	new file:   app/meals/[slug]/page.module.css
    	modified:   tailwind.config.js

--- app/meals/[slug]/page.jsx
+++ app/meals/[slug]/page.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import Image from 'next/image';
+import classes from '@/app/meals/[slug]/page.module.css';
 
 const MealPage = () => {
   return (
-    <div>MealPage</div>
-  )
-}
+    <>
+      <header className={classes.header}>
+        <div className={classes.image}>
+          <Image fill />
+        </div>
+        <div className={classes.headerText}>
+          <h1>Title</h1>
+          <p className={classes.creator}>
+            by <a href={`mailto:${'EMAIL'}`}>NAME</a>
+          </p>
+          <p className={classes.summary}>SUMMARY</p>
+        </div>
+      </header>
+      <main>
+        <p
+          className={classes.instructions}
+          dangerouslySetInnerHTML={{
+            __html: '...',
+          }}
+        ></p>
+      </main>
+    </>
+  );
+};
 
-export default MealPage
+export default MealPage;
--- tailwind.config.js
+++ tailwind.config.js
@@ -13,7 +13,56 @@ module.exports = {
       fontFamily: {
         Montserrat: ["Montserrat", "sans-serif"],
         Quicksand: ['Quicksand', "sans-serif"]
-      }
+      },
+      animation: {
+        'fade-slide-in-from-left': 'fade-slide-in-from-left 1s ease-out forwards',
+        'fade-slide-in-from-right': 'fade-slide-in-from-right 1s ease-out forwards',
+        'fade-slide-in-from-bottom': 'fade-slide-in-from-bottom 1s ease-out forwards',
+        'flash': 'flash 1s ease-in-out infinite alternate',
+      },
+      keyframes: {
+        'fade-slide-in-from-left': {
+          '0%': {
+            opacity: '0',
+            transform: 'translateX(-1rem)', // Adjust as per your design
+          },
+          '100%': {
+            opacity: '1',
+            transform: 'translateX(0)',
+          },
+        },
+        'fade-slide-in-from-right': {
+          '0%': {
+            opacity: '0',
+            transform: 'translateX(1rem)',
+          },
+          '100%': {
+            opacity: '1',
+            transform: 'translateX(0)',
+          }
+        },
+        'fade-slide-in-from-bottom': {
+          '0%': {
+            opacity: '0',
+            transform: 'translateY(1rem)',
+          },
+          '100%': {
+            opacity: '1',
+            transform: 'translateY(0)',
+          }
+        },
+        'flash': {
+          '0%': {
+            opacity: '0'
+          },
+          '50%': {
+            opacity: '1'
+          },
+          '100%': {
+            opacity: '0'
+          },
+        }
+      },
     },
   },
   plugins: [],
